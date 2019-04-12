4e0dac013157cd4d91fe42897c199e1e7c4960ba
Pyotr Bodlev <41431202+pyotrbodlev@users.noreply.github.com>
2019-04-12 14:24:32 +0300

    Exam Prep - Panzer

--- JavaOOPAdvanced/Panzer/src/panzer/constants/Text.java
+++ JavaOOPAdvanced/Panzer/src/panzer/constants/Text.java
@@ -4,4 +4,5 @@ public final class Text {
 
     public static final String VEHICLE_CREATED = "Created %s Vehicle – %s";
     public static final String PART_CREATED = "Added %s - %s to Vehicle - %s";
+    public static final String BATTLE_COMMAND_OUTPUT = "%s versus %s -> %s Wins! Flawless Victory!";
 }
--- JavaOOPAdvanced/Panzer/src/panzer/core/PanzerManager.java
+++ JavaOOPAdvanced/Panzer/src/panzer/core/PanzerManager.java
@@ -8,21 +8,20 @@ import panzer.contracts.Vehicle;
 import panzer.factories.PartFactory;
 import panzer.factories.VehicleFactory;
 
-import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PanzerManager implements Manager {
-    private static final String VEHICLES_PATH = "panzer.models.vehicles.";
-
     private Map<String, Vehicle> vehicles;
+    private Map<String, Vehicle> defeatedVehicles;
     private Map<String, Part> parts;
     private BattleOperator battleOperator;
 
     public PanzerManager() {
         this.vehicles = new LinkedHashMap<>();
         this.parts = new LinkedHashMap<>();
+        this.defeatedVehicles = new LinkedHashMap<>();
         this.battleOperator = new PanzerBattleOperator();
     }
 
@@ -41,9 +40,9 @@ public class PanzerManager implements Manager {
 
         Part part = PartFactory.createPart(arguments);
 
-        if (this.vehicles.containsKey(arguments.get(1))){
+        if (this.vehicles.containsKey(arguments.get(1))) {
             Vehicle currentVehicle = this.vehicles.get(arguments.get(1));
-            switch (partType){
+            switch (partType) {
                 case "Shell":
                     currentVehicle.addShellPart(part);
                     break;
@@ -68,7 +67,23 @@ public class PanzerManager implements Manager {
 
     @Override
     public String battle(List<String> arguments) {
-        return null;
+        String attacker = arguments.get(1);
+        String target = arguments.get(2);
+
+        Vehicle attackerVehicle = this.vehicles.get(attacker);
+        Vehicle targetVehicle = this.vehicles.get(target);
+
+        String winner = this.battleOperator.battle(attackerVehicle, targetVehicle);
+
+        if (winner.equals(attacker)) {
+            this.defeatedVehicles.put(target, targetVehicle);
+            this.vehicles.remove(target);
+        } else {
+            this.defeatedVehicles.put(attacker, attackerVehicle);
+            this.vehicles.remove(attacker);
+        }
+
+        return String.format(Text.BATTLE_COMMAND_OUTPUT, attacker, target, winner);
     }
 
     @Override
