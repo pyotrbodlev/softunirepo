c87cf5b40f1c23a14ac8a480cdcaf39483153915
Pyotr Bodlev <pyotr.bodlev@abv.bg>
2019-04-29 11:09:07 +0300

    Commit all folders

---
+++ JavaDBAdvanced/HibernateCodeFirst/hibernatecodefirst/src/main/java/gringotts/app/entities/WizardDeposits.java
@@ -0,0 +1,172 @@
+package gringotts.app.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "wizards_deposits")
+public class WizardDeposits {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String notes;
+    private int age;
+    private String magicWandCreator;
+    private int magicWandSize;
+    private String depositGroup;
+    private Date depositStartDate;
+    private BigDecimal depositAmount;
+    private BigDecimal depositInterest;
+    private BigDecimal depositCharge;
+    private Date depositExpirationDate;
+    private boolean isDepositExpired;
+
+    public WizardDeposits() {}
+
+    public WizardDeposits(String firstName, String lastName, int age) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", length = 50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(length = 1000)
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Column(nullable = false)
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age can't be negative number");
+        }
+
+        this.age = age;
+    }
+
+    @Column(name = "magic_wand_creator", length = 100)
+    public String getMagicWandCreator() {
+        return magicWandCreator;
+    }
+
+    public void setMagicWandCreator(String magic_wand_creator) {
+        this.magicWandCreator = magic_wand_creator;
+    }
+
+    @Column(name = "magic_wand_size")
+    public int getMagicWandSize() {
+        return magicWandSize;
+    }
+
+    public void setMagicWandSize(int magic_wand_size) {
+        if (magic_wand_size < 0) {
+            throw new IllegalArgumentException("Size can't be negative number");
+        }
+
+        this.magicWandSize = magic_wand_size;
+    }
+
+    @Column(name = "deposit_group", length = 20)
+    public String getDepositGroup() {
+        return depositGroup;
+    }
+
+    public void setDepositGroup(String deposit_group) {
+        this.depositGroup = deposit_group;
+    }
+
+    @Column(name = "deposit_start_date")
+    public Date getDepositStartDate() {
+        return depositStartDate;
+    }
+
+    public void setDepositStartDate(Date deposit_start_date) {
+        this.depositStartDate = deposit_start_date;
+    }
+
+    @Column(name = "deposit_amount")
+    public BigDecimal getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(BigDecimal deposit_amount) {
+
+        if (deposit_amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Amount can't be negative number");
+        }
+
+        this.depositAmount = deposit_amount;
+    }
+
+    @Column(name = "deposit_interest")
+    public BigDecimal getDepositInterest() {
+        return depositInterest;
+    }
+
+    public void setDepositInterest(BigDecimal deposit_interest) {
+        this.depositInterest = deposit_interest;
+    }
+
+    @Column(name = "deposit_charge")
+    public BigDecimal getDepositCharge() {
+        return depositCharge;
+    }
+
+    public void setDepositCharge(BigDecimal deposit_charge) {
+        this.depositCharge = deposit_charge;
+    }
+
+    @Column(name = "deposit_expiration_date")
+    public Date getDepositExpirationDate() {
+        return depositExpirationDate;
+    }
+
+    public void setDepositExpirationDate(Date deposit_expiration_date) {
+        this.depositExpirationDate = deposit_expiration_date;
+    }
+
+    @Column(name = "is_deposit_expired")
+    public boolean isIsDepositExpired() {
+        return isDepositExpired;
+    }
+
+    public void setIsDepositExpired(boolean is_deposit_expired) {
+        this.isDepositExpired = is_deposit_expired;
+    }
+}
---
+++ JavaDBAdvanced/HibernateCodeFirst/hibernatecodefirstuniveristysystem/src/main/java/univeristy_system/app/entities/Person.java
@@ -0,0 +1,49 @@
+package univeristy_system.app.entities;
+
+import univeristy_system.app.utils.Idable;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Person extends Idable {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Column(length = 25)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(length = 30)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(length = 15)
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
