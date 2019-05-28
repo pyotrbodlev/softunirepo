042f26c09e1a830c4790284e9dc1d04fe7767992
Pyotr Bodlev <41431202+pyotrbodlev@users.noreply.github.com>
2019-05-28 21:23:16 +0300

    Imported new project - Most Wanted. Exam prep Java DB Advaneced.

---
+++ JavaDBAdvanced/mostwanted/src/main/java/mostwanted/MostwantedApplication.java
@@ -0,0 +1,13 @@
+package mostwanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostwantedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MostwantedApplication.class, args);
+    }
+
+}
