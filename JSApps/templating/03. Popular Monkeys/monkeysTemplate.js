59e4a5b62915c9d05d337ad19794208540b1204f
Pyotr Bodlev <41431202+pyotrbodlev@users.noreply.github.com>
2019-08-24 19:19:15 +0300

    commit third problem;

--- JSApps/templating/03. Popular Monkeys/monkeysTemplate.js
+++ JSApps/templating/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,14 @@
 $(() => {
-    // TODO
+    const listTemplate = `{{#each monkeys}}{{> monkey}}{{/each}}`;
+
+    const monkeyTemplate = document.getElementById('monkey-template').innerHTML;
+    Handlebars.registerPartial('monkey', monkeyTemplate);
+
+    const template = Handlebars.compile(listTemplate);
+
+    const html = template({
+        monkeys 
+    });
+
+    document.querySelector('.monkeys').innerHTML = html;
 })
