22cd995617bbf7e631143b54ccb5ed1651a6e12d
pyotrbodlev <pyotr.bodlev@atia.com>
2020-02-28 12:07:44 +0200

    delete node modules

--- JSWeb/NodeJS/MongoPlayground/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectionString = 'mongodb://127.0.0.1:27017/test_db';
-mongoose.connect(connectionString);
-
-//module.exports = mongoose.connect(connectionString)
--- JSWeb/NodeJS/intro-to-node-js/index.js
+++
@@ -1,6 +0,0 @@
-const storage = require('./storage');
-
-storage.load();
-const all = storage.getAll();
-
-console.log(all);
